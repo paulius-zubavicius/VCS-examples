dfb0d1a325395b76facd1e21c7ac13e6b134b458
Paulius Zubavičius <paulius@zubavicius.lt>
2019-05-23 22:38:32 +0300

    Refactoring shoot smoke effect animation

--- game-2DShooter/src/main/java/com/vcs/ex/jfx/game/layers/l2_obs/ObstaclesLayer.java
+++ game-2DShooter/src/main/java/com/vcs/ex/jfx/game/layers/l2_obs/ObstaclesLayer.java
@@ -1,164 +1,135 @@
 package com.vcs.ex.jfx.game.layers.l2_obs;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import com.vcs.ex.jfx.game.layers.GraphicalLayer;
 import com.vcs.ex.jfx.game.resources.ImgRs;
 import com.vcs.ex.jfx.game.shotimpact.ShotImpact;
 import com.vcs.ex.jfx.game.utils.ImgLoader;
-
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ObstaclesLayer implements GraphicalLayer {
 
-	private static final double WIND_SPEED = 0.1;
-	private static final double SEC = 1000000000.0;
-	private static final long FX_SPEED = 3000000L;
+    private static final double WIND_SPEED = 0.1;
+    private static final double SEC = 1000000000.0;
+    private static final long FX_SPEED = 3000000L;
 
-	private Image cloud;
-	private GraphicsContext gc;
-	private long startNanoTime;
-	private Random rnd;
-	private Image[][] bulletSmoke = null;
+    private Image cloud;
+    private GraphicsContext gc;
+    private long startNanoTime;
+    private Random rnd;
+    private Image[][] bulletSmoke = null;
 
-	private double w;
-	private double h;
-	private double centerOffsetX;
-	private double centerOffsetY;
+    private double w;
+    private double h;
+    private double centerOffsetX;
+    private double centerOffsetY;
 
-	private ShootSmokePos fxPos;
+    private List<ShootSmokePos> shootSmokePosPool = new ArrayList<>();
 
-	private List<ShootSmokePos> shootSmokePosPool = new ArrayList<>();
+    private double x, y;
 
-	private double x, y;
 
-	@Override
-	public void init(long startNanoTime, GraphicsContext gc) {
-		this.gc = gc;
-		this.startNanoTime = startNanoTime;
-		this.cloud = ImgLoader.img(ImgRs.CLOUD2);
+    @Override
+    public void init(long startNanoTime, GraphicsContext gc) {
+        this.gc = gc;
+        this.startNanoTime = startNanoTime;
+        this.cloud = ImgLoader.img(ImgRs.CLOUD2);
 
-		bulletSmoke = new Image[4][];
+        bulletSmoke = new Image[4][];
 
-		bulletSmoke[0] = ImgLoader.sprite(ImgRs.SMOKE1_1);
-		bulletSmoke[1] = ImgLoader.sprite(ImgRs.SMOKE2_1);
-		bulletSmoke[2] = ImgLoader.sprite(ImgRs.SMOKE2_2);
-		bulletSmoke[3] = ImgLoader.sprite(ImgRs.SMOKE3_1);
+        bulletSmoke[0] = ImgLoader.sprite(ImgRs.SMOKE1_1);
+        bulletSmoke[1] = ImgLoader.sprite(ImgRs.SMOKE2_1);
+        bulletSmoke[2] = ImgLoader.sprite(ImgRs.SMOKE2_2);
+        bulletSmoke[3] = ImgLoader.sprite(ImgRs.SMOKE3_1);
 
-		w = bulletSmoke[0][0].getWidth();
-		h = bulletSmoke[0][0].getHeight();
+        w = bulletSmoke[0][0].getWidth();
+        h = bulletSmoke[0][0].getHeight();
 
-		centerOffsetX = w / 2;
-		centerOffsetY = h / 2;
+        centerOffsetX = w / 2;
+        centerOffsetY = h / 2;
 
-		rnd = new Random(startNanoTime);
-	}
+        rnd = new Random(startNanoTime);
+    }
 
-	@Override
-	public void reset() {
-		x = 0;
-		y = 0;
-		gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
-		gc.drawImage(cloud, 0, 0);
-	}
+    @Override
+    public void reset() {
+        x = 0;
+        y = 0;
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+        gc.drawImage(cloud, 0, 0);
+    }
 
-	@Override
-	public void updateTimer(long currentNanoTime) {
-		double t = (currentNanoTime - startNanoTime) / SEC;
+    @Override
+    public void updateTimer(long currentNanoTime) {
 
-		// gc.clearRect(x, y, cloud.getWidth(), cloud.getHeight());
+        // Clear the smoke effects before repaint & remove old ones
+        shootSmokePosPool.removeIf(this::isNoNextSprite);
+        shootSmokePosPool.stream()
+                .filter(fxPos -> isItTimeForNextSprite(fxPos, currentNanoTime))
+                .forEach(fxPos -> fxPos.spriteIndex++);
+        shootSmokePosPool.forEach(fxPos -> fxPos.lastTime = currentNanoTime);
 
-		gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+//        System.out.println(shootSmokePosPool.size());
 
-		// boolean fxFlag = false;
 
-		// Clear the smoke effects before repaint & remove old ones
-		for (int i = 0; i < shootSmokePosPool.size(); i++) {
-			fxPos = shootSmokePosPool.get(i);
+        x += WIND_SPEED;
+        y = 128 * Math.sin(((currentNanoTime - startNanoTime) / SEC) * WIND_SPEED);
+        if (x > gc.getCanvas().getWidth()) {
+            x = -cloud.getWidth();
+        }
 
-			// gc.clearRect(fxPos.x - centerOffsetX - fxPos.cloudOffsetX + x,
-			// fxPos.y - centerOffsetY - fxPos.cloudOffsetY + y, w, h);
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
 
-			if (fxPos.valid) {
+        gc.drawImage(cloud, x, y);
+        shootSmokePosPool.forEach(pos -> gc.drawImage(bulletSmoke[pos.spriteFxIndex][pos.spriteIndex], pos.x - centerOffsetX - pos.cloudOffsetX + x,
+                pos.y - centerOffsetY - pos.cloudOffsetY + y, w, h));
 
-				if (currentNanoTime - fxPos.lastTime > FX_SPEED) {
-					if (fxPos.spriteIndex < bulletSmoke[fxPos.spriteFxIndex].length - 1) {
-						fxPos.spriteIndex = fxPos.spriteIndex + 1;
-						// fxFlag = true;
-					} else {
-						fxPos.valid = false;
-					}
-				}
 
-				fxPos.lastTime = currentNanoTime;
-			} else {
-				shootSmokePosPool.remove(i);
-				i--;
-			}
+    }
 
-		}
+    private boolean isItTimeForNextSprite(ShootSmokePos fxPos, long currentNanoTime) {
+        return currentNanoTime - fxPos.lastTime > FX_SPEED;
+    }
 
-		// for (ShootSmokePos pos : shootSmokePosPool) {
-		// gc.clearRect(pos.x - centerOffsetX - pos.cloudOffsetX + x, pos.y -
-		// centerOffsetY - pos.cloudOffsetY + y, w,
-		// h);
-		// }
+    private boolean isNoNextSprite(ShootSmokePos fxPos) {
+        return fxPos.spriteIndex >= bulletSmoke[fxPos.spriteFxIndex].length - 1;
+    }
 
-		x += WIND_SPEED;
-		y = 128 * Math.sin(t * WIND_SPEED);
 
-		gc.drawImage(cloud, x, y);
+    @Override
+    public ShotImpact shotImpact(double x, double y, ShotImpact impact) {
 
-		// if (fxFlag) {
-		for (ShootSmokePos pos : shootSmokePosPool) {
-			gc.drawImage(bulletSmoke[pos.spriteFxIndex][pos.spriteIndex], pos.x - centerOffsetX - pos.cloudOffsetX + x,
-					pos.y - centerOffsetY - pos.cloudOffsetY + y, w, h);
-		}
-		// }
+        shootSmokePosPool.add(new ShootSmokePos(x, y, this.x, this.y, rnd.nextInt(bulletSmoke.length)));
 
-		if (x > gc.getCanvas().getWidth()) {
-			x = -cloud.getWidth();
-		}
+        return impact;
+    }
 
-	}
+    private class ShootSmokePos {
 
-	@Override
-	public ShotImpact shotImpact(double x, double y, ShotImpact impact) {
+        private double x;
+        private double y;
 
-		shootSmokePosPool.add(new ShootSmokePos(x, y, this.x, this.y, rnd.nextInt(bulletSmoke.length)));
+        private double cloudOffsetX;
+        private double cloudOffsetY;
 
-		return impact;
-	}
+        private int spriteIndex;
+        private int spriteFxIndex;
+        private long lastTime;
 
-	private class ShootSmokePos {
+        public ShootSmokePos(double x, double y, double cloudOffsetX, double cloudOffsetY, int spriteFxIndex) {
+            this.x = x;
+            this.y = y;
+            this.cloudOffsetX = cloudOffsetX;
+            this.cloudOffsetY = cloudOffsetY;
+            this.spriteIndex = -1;
+            this.spriteFxIndex = spriteFxIndex;
+            this.lastTime = System.currentTimeMillis();
+        }
 
-		private boolean valid;
-
-		private double x;
-		private double y;
-
-		private double cloudOffsetX;
-		private double cloudOffsetY;
-
-		private int spriteIndex;
-		private int spriteFxIndex;
-		private long lastTime;
-
-		public ShootSmokePos(double x, double y, double cloudOffsetX, double cloudOffsetY, int spriteFxIndex) {
-			this.valid = true;
-			this.x = x;
-			this.y = y;
-			this.cloudOffsetX = cloudOffsetX;
-			this.cloudOffsetY = cloudOffsetY;
-			this.spriteIndex = -1;
-			this.spriteFxIndex = spriteFxIndex;
-			this.lastTime = System.currentTimeMillis();
-
-		}
-
-	}
+    }
 
 }
